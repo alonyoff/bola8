40d0da4ffd37cac25be559db8f122c1891e51d09
alonyoff <117363959+alonyoff@users.noreply.github.com>
2024-07-23 19:14:54 -0300

    Delete bola8 directory

--- bola8/bola8/script.js
+++
@@ -1,6 +0,0 @@
-let respostas = ["Sim", "Não", "Talvez", "Claro que sim", "Claro que não","Obvio que sim", "Obvio que não", "Hoje não","Amanhã","Essa pergunta é muito importante para ser respondida aleatoriamente"]
-
-function escolheRespostas(){
-    let pegaResposta = respostas[Math.floor(Math.random() * respostas.length)];
-    document.querySelector(".bola8").innerHTML = pegaResposta;
-}
